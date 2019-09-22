d26b0f4838b2cfc33adf64d551760d587e214aac
Istvan Vincze <pilu110@gmail.com>
2019-09-23 00:29:31 +0200

    center on click

--- src/main/java/com/pilu/mundi/ui/ViewPort.java
+++ src/main/java/com/pilu/mundi/ui/ViewPort.java
@@ -1,6 +1,7 @@
 package com.pilu.mundi.ui;
 
 import java.awt.Point;
+import java.awt.geom.Point2D;
 
 import com.pilu.mundi.entity.ComplexSequenceMatrix;
 import com.pilu.mundi.generator.Generator;
@@ -9,12 +10,12 @@ import org.apache.commons.math3.complex.Complex;
 public class ViewPort {
 
     private final Generator generator;
-    private Point origo;
+    private Point2D.Double origo;
     private int width;
     private int height;
     private double scale;
 
-    public ViewPort(Generator generator, Point origo, double scale, int width, int height) {
+    public ViewPort(Generator generator, Point2D.Double origo, double scale, int width, int height) {
         this.generator = generator;
         this.origo = origo;
         this.scale = scale;
@@ -23,7 +24,8 @@ public class ViewPort {
     }
 
     public void center(Point point) {
-        origo = new Point(point.x - width / 2, point.y - height / 2);
+        origo = new Point2D.Double(scale * ((double)point.x - width / 2.0) + origo.x,
+                scale * ((double)point.y - height / 2.0) + origo.y);
     }
 
     public ComplexSequenceMatrix render() {
@@ -33,8 +35,8 @@ public class ViewPort {
         for(int j = 0; j < height; j++) {
             for(int i = 0; i < width; i++) {
 
-                double x = scale * (i - width / 2.0 + origo.x);
-                double y = scale * (j - height / 2.0 + origo.y);
+                double x = scale * (i - width / 2.0) + origo.x;
+                double y = scale * (j - height / 2.0) + origo.y;
 
                 matrix.set(i, j, generator.generate(new Complex(x, y)));
             }
