e4fd3c6962678d1b79c0e0f919eb68b93b3ba840
Istvan Vincze <pilu110@gmail.com>
2019-10-03 01:15:49 +0200

    nth root fractal

--- src/main/java/com/pilu/mundi/MundiMain.java
+++ src/main/java/com/pilu/mundi/MundiMain.java
@@ -3,6 +3,7 @@ package com.pilu.mundi;
 import java.awt.Point;
 import java.awt.geom.Point2D;
 
+import com.pilu.mundi.generator.M2Generator;
 import com.pilu.mundi.ui.ViewPort;
 import com.pilu.mundi.ui.colorer.AvgColorer;
 import com.pilu.mundi.ui.colorer.Colorer;
@@ -13,8 +14,9 @@ import com.pilu.mundi.ui.GraphicUI;
 
 public class MundiMain {
   public static void main(String [] args) {
-    MandelGenerator mandelGenerator = new MandelGenerator(2);
-    ViewPort viewPort = new ViewPort(mandelGenerator, new Point2D.Double(0,0), 4.0 / 800, 500, 500);
+    M2Generator mandelGenerator = new M2Generator(3,2);
+    ViewPort viewPort = new ViewPort(mandelGenerator, new Point2D.Double(0,0), 4.0 / 800, 600, 600);
+    //Colorer colorer = new SequenceSizeColorer(0.15f, 0.9f);
     Colorer colorer = new AvgColorer(0.15f, 0.9f);
     GraphicUI graphicUI = new GraphicUI(viewPort, colorer);
     graphicUI.display();
